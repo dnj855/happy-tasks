0e8f2e1c44dc40bc398217eaa26bfe6e01b096ef
Guillaume Fournier <fournierg01@gmail.com>
2024-12-09 18:00:35 +0100

    validations

--- app/models/task_type.rb
+++ app/models/task_type.rb
@@ -1,3 +1,5 @@
 class TaskType < ApplicationRecord
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
+
+  validates_inclusion_of :name, :in => %w( chambre linge ménage devoirs animaux sortir )
 end
