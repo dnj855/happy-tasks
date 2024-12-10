49a5743e980b61ef9dae38d5af3230797354c44f
Helene Demanet <demanet.helene@gmail.com>
2024-12-10 21:46:03 +0100

    First work about family dashboard

--- app/models/child.rb
+++ app/models/child.rb
@@ -3,10 +3,10 @@ class Child < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_many :awards, dependent: :destroy
 
-validates :first_name, presence: true
-validates :age, presence: true, numericality: { only_numeric: true, greater_than_or_equal_to: 3 }
-validates :day_points,  numericality: { only_numeric: true }
-validates :week_points, numericality: { only_numeric: true }
-validates :month_points, numericality: { only_numeric: true }
+  validates :first_name, presence: true
+  validates :age, presence: true, numericality: { only_numeric: true, greater_than_or_equal_to: 3 }
+  validates :day_points,  numericality: { only_numeric: true }
+  validates :week_points, numericality: { only_numeric: true }
+  validates :month_points, numericality: { only_numeric: true }
 
 end
