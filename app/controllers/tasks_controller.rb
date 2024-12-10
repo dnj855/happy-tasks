1156fa0fb3f1219b237e04e9ab5e5887c76e0a7f
Emilie Vennat <emilie.vennat@gmail.com>
2024-12-10 22:51:15 +0100

    begin task controller and new form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,35 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[edit update destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to child_path(@task.child)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    @task.update(task_params)
+    redirect_to tasks_path
   end
 
   def destroy
+    @task.destroy
+    redirect_to tasks_path, notice: 'Tâche supprimée'
   end
 
   def declare_done
@@ -20,7 +38,13 @@ class TasksController < ApplicationController
   def validate
   end
 
-  def index
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :value, :task_type_id, :child_id)
   end
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
