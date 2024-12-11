af83e80ae9c426a226d5f1cca1c128d9d7927e5c
Guillaume Fournier <fournierg01@gmail.com>
2024-12-11 10:26:29 +0100

    amelio/refacto seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,18 +4,9 @@ require 'faker'
 require 'uri'
 require 'net/http'
 
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
+# URL email
 URL_PATH = '@happy-tasks.click'
-
+# Super users
 BASE_USERS = [
   { first_name: "Guillaume", last_name: "Fournier", email: "gf#{URL_PATH}", child: false, password: "happyT"},
   { first_name: "Hélène", last_name: "Demanet", email: "hd#{URL_PATH}", child: false, password: "happyT"},
@@ -26,14 +17,58 @@ BASE_USERS = [
   { first_name: "Monsieur", last_name: "Vennat-Louveau", email: "mvl#{URL_PATH}", child: false, password: "happyT"},
   { first_name: "Madame", last_name: "Lang-Roth", email: "mlr#{URL_PATH}", child: false, password: "happyT"},
 ]
-
+# Types de tâches
 TASK_TYPES = [
   "chambre", "linge", "ménage", "cuisine", "animaux", "apprentissage"
 ]
-
+# liste des tâches
+TASK_LIST = {
+  chambre: ["Ranger la chambre", "Aérer la chambre", "Faire son lit", "Réorganiser sa chambre", "Préparer sa chambre"],
+  linge: ["Ranger le linge", "Trier le linge", "Plier le linge", "Etendre le linge", "Apporter le linge"],
+  ménage: ["Passer l'aspirateur", "Faire les vitres", "Passer le balai"],
+  cuisine: ["Débarrasser le LV", "Mettre au LV", "Mettre la table", "Dabarrasser la table", "Cuisiner" ],
+  animaux: ["Nourrir les animaux", "Sortir les animaux", "Entretenir les animaux"],
+  apprentissage: ["Faire ses devoirs", "Prendre sa douche", "Se brosser les dents", "Aider aux devoirs", "Pratiquer une activité", "Jardiner"]
+}
+# types de récompenses/privilèges
 AWARD_TYPES = [
   "quotidien", "hebdomadaire", "mensuel"
 ]
+# liste des récompenses/privilèges
+AWARD_LIST = {
+  quotidien: ["Gagner 10 min d'écran", "Jeu de société rapide", "Gagner 10 min de télé"],
+  hebdomadaire: ["Jeu de société en famille", "Inviter quelqu'un", "Sortir au cinéma avec quelqu'un"],
+  mensuel: ["Sortir au restaurant", "Acheter des livres", "Sortir au cinéma en famille"]
+}
+# Age mini pour l'appli
+MIN_AGE = 4
+# Age maxi pour l'appli
+MAX_AGE = 16
+# Age pour avoir accès soi-même
+AGE_ACCOUNT = 9
+# Les points que
+MIN_DAY_POINTS = 30
+MAX_DAY_POINTS = 90
+MIN_WEEK_POINTS = 40
+MAX_WEEK_POINTS = 120
+MIN_MONTH_POINTS = 60
+MAX_MONTH_POINTS = 180
+MIN_POINTS_TASK = 20
+MAX_POINTS_TASK = 50
+# points minimum pour un(e) récompense/privilège
+MIN_AWARD_POINTS = {
+  quotidien: MIN_POINTS_TASK / 3,
+  hebdomadaire: MIN_POINTS_TASK / 2,
+  mensuel: MIN_POINTS_TASK
+}
+# points maximum pour un(e) récompense/privilège
+MAX_AWARD_POINTS = {
+  quotidien: MAX_POINTS_TASK / 3,
+  hebdomadaire: MAX_POINTS_TASK / 2,
+  mensuel: MAX_POINTS_TASK
+}
+
+
 
 puts "---Destroying all data"
 User.destroy_all
@@ -52,90 +87,47 @@ TASK_TYPES.each do |type|
 end
 puts "---6 Task_Types CREATED"
 
-puts "---Creating base users"
-BASE_USERS.take(4).each do |base_user|
-  family = Family.create!(name: base_user[:last_name])
-  User.create!(family_id: family.id, first_name: base_user[:first_name], last_name: base_user[:last_name], email: base_user[:email], child: base_user[:child], password: base_user[:password])
+
+def create_the_rest(family)
   3.times do
     child = Child.create!(
        family_id: family.id,
        first_name: Faker::Name.first_name,
-       age: (4..22).to_a.sample,
-       day_points: (35..92).to_a.sample,
-       week_points: (35..92).to_a.sample,
-       month_points: (35..92).to_a.sample,)
-    if child.age > 9
+       age: (MIN_AGE..MAX_AGE).to_a.sample,
+       day_points: (MIN_DAY_POINTS..MAX_DAY_POINTS).to_a.sample,
+       week_points: (MIN_WEEK_POINTS..MAX_WEEK_POINTS).to_a.sample,
+       month_points: (MIN_MONTH_POINTS..MAX_MONTH_POINTS).to_a.sample,)
+    if child.age >= AGE_ACCOUNT
       User.create!(family_id: family.id, child_id: child.id, first_name: child.first_name, last_name: family.name, email: Faker::Internet.email, password: "happyT", child: true)
     else
       User.create!(family_id: family.id, child_id: child.id, first_name: child.first_name, last_name: family.name, email: Faker::Internet.email, password: "happyT", child: false)
     end
-    award = Award.create!(child_id: child.id, name: "Jouer à la console", periodicity: AWARD_TYPES.sample, value: (50..250).to_a.sample)
+    award_type = AWARD_TYPES.sample
+    award = Award.create!(child_id: child.id, name: AWARD_LIST[award_type.to_sym].sample, periodicity: award_type, value: (MIN_AWARD_POINTS[award_type.to_sym]..MAX_AWARD_POINTS[award_type.to_sym]).to_a.sample)
     4.times do
-      task_type = TaskType.all.sample
-      case task_type.name
-      when "chambre"
-        name= ["Ranger la chambre", "Aérer la chambre", "Faire son lit", "Réorganiser sa chambre", "Préparer sa chambre"].sample
-      when "linge"
-        name= ["Ranger le linge", "Trier le linge", "Plier le linge", "Etendre le linge", "Apporter le linge"].sample
-      when "ménage"
-        name= ["Passer l'aspirateur", "Faire les vitres", "Passer le balai"].sample
-      when "cuisine"
-        name= ["Débarrasser le LV", "Mettre au LV", "Mettre la table", "Dabarrasser la table", "Cuisiner" ].sample
-      when "animaux"
-        name= ["Nourrir les animaux", "Sortir les animaux", "Entretenir les animaux", "Enterrer les animaux"].sample
-      when "apprentissage"
-        name= ["Faire ses devoirs", "Prendre sa douche", "Se brosser les dents", "Aider aux devoirs", "Pratiquer une activité", "Jardiner"].sample
-      else
-        "Erreur: ce type de tâche n'existe pas"
-      end
-      task = Task.create!(child_id: child.id, task_type: task_type, name: name, value: (30..100).to_a.sample)
+      task_type = TASK_TYPES.sample
+      Task.create!(child_id: child.id, task_type_id: TaskType.find_by(name: task_type).id, name: TASK_LIST[task_type.to_sym].sample, value: (MIN_POINTS_TASK..MAX_POINTS_TASK).to_a.sample)
     end
   end
+end
+
+
+puts "---Creating base users"
+BASE_USERS.take(4).each do |base_user|
+  family = Family.create!(name: base_user[:last_name])
+  User.create!(family_id: family.id, first_name: base_user[:first_name], last_name: base_user[:last_name], email: base_user[:email], child: base_user[:child], password: base_user[:password])
+  create_the_rest(family)
 end
 puts "---Base users CREATED"
 
 puts "---Creating 50 other families"
 
 50.times do
-  family = Family.create!(name: Faker::Name.name)
+  family = Family.create!(name: Faker::Name.last_name)
   first_name1 = Faker::Name.first_name
   first_name2 = Faker::Name.first_name
   user1 = User.create!(family_id: family.id, first_name: first_name1, last_name: family.name, email: Faker::Internet.email(name: first_name1), child: false, password: 'happyT')
   user2 = User.create!(family_id: family.id, first_name: first_name2, last_name: family.name, email: Faker::Internet.email(name: first_name2), child: false, password: 'happyT')
-  3.times do
-    child = Child.create!(
-       family_id: family.id,
-       first_name: Faker::Name.first_name,
-       age: (4..22).to_a.sample,
-       day_points: (35..92).to_a.sample,
-       week_points: (35..92).to_a.sample,
-       month_points: (35..92).to_a.sample,)
-    if child.age > 9
-      User.create!(family_id: family.id, child_id: child.id, first_name: child.first_name, last_name: family.name, email: Faker::Internet.email(name: child.first_name), password: "happyT", child: true)
-    else
-      User.create!(family_id: family.id, child_id: child.id, first_name: child.first_name, last_name: family.name, email: Faker::Internet.email(name: child.first_name), password: "happyT", child: false)
-    end
-    award = Award.create!(child_id: child.id, name: "Jouer à la console", periodicity: AWARD_TYPES.sample, value: (50..250).to_a.sample)
-    4.times do
-      task_type = TaskType.all.sample
-      case task_type.name
-      when "chambre"
-        name= ["Ranger la chambre", "Aérer la chambre", "Faire son lit", "Réorganiser sa chambre", "Préparer sa chambre"].sample
-      when "linge"
-        name= ["Ranger le linge", "Trier le linge", "Plier le linge", "Etendre le linge", "Apporter le linge"].sample
-      when "ménage"
-        name= ["Passer l'aspirateur", "Faire les vitres", "Passer le balai"].sample
-      when "cuisine"
-        name= ["Débarrasser le LV", "Mettre au LV", "Mettre la table", "Dabarrasser la table", "Cuisiner" ].sample
-      when "animaux"
-        name= ["Nourrir les animaux", "Sortir les animaux", "Entretenir les animaux", "Enterrer les animaux"].sample
-      when "apprentissage"
-        name= ["Faire ses devoirs", "Prendre sa douche", "Se brosser les dents", "Aider aux devoirs", "Pratiquer une activité", "Jardiner"].sample
-      else
-        "Erreur: ce type de tâche n'existe pas"
-      end
-      task = Task.create!(child_id: child.id, task_type: task_type, name: name, value: (30..100).to_a.sample)
-    end
-  end
+  create_the_rest(family)
 end
 puts "---50 Families CREATED"
