d298fcc519e4a3c0e392f914e0652cea3942974a
Helene Demanet <demanet.helene@gmail.com>
2024-12-11 13:07:20 +0100

    first work without css

--- app/controllers/awards_controller.rb
+++ app/controllers/awards_controller.rb
@@ -1,8 +1,25 @@
 class AwardsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_award, only: [:edit, :update, :destroy]
+
   def new
+    @award = Award.new
+    @children = current_user.family.children
+
+    if params[:child_id]
+      @award.child_id = current_user.family.children.find_by(id: params[:child_id])
+    end
   end
 
   def create
+    @award = Award.new(award_params)
+
+    if @award.save
+      redirect_to family_dashboard_path, notice: 'Le privilège a bien été créé.'
+    else
+      @children = current_user.family.children
+      render :new
+    end
   end
 
   def edit
@@ -12,6 +29,23 @@ class AwardsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_award
+    @award = Award.find(params[:id])
+    authorize_award
+  end
+
+  def award_params
+    params.require(:award).permit(:name, :value, :child_id)
+  end
+
+  def authorize_award
+    unless current_user.family == @award.child.family
+      redirect_to family_dashboard_path, alert: 'Vous ne pouvez pas modifier ce privilège.'
+    end
   end
 
 end
