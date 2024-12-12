738e35cc7d5264e430d23acdd375212419af52e8
Emilie Vennat <emilie.vennat@gmail.com>
2024-12-12 15:04:12 +0100

    form new child

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,2 +1,24 @@
 class ChildrenController < ApplicationController
+  def new
+    @child = Child.new
+    authorize @child
+  end
+
+  def create
+    @child = Child.new(child_params)
+    @child.family = current_user.family
+    authorize @child
+
+    if @child.save
+      redirect_to family_dashboard_path(@child.family), notice: 'Votre enfant a été enregistré .'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def child_params
+    params.require(:child).permit(:first_name, :age)
+  end
 end
