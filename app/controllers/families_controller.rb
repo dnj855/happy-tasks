5147b12b520f99308d3947bffa3f929d09dd3a1d
Emilie Vennat <emilie.vennat@gmail.com>
2024-12-12 12:15:11 +0100

    new family form

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,4 +1,24 @@
 class FamiliesController < ApplicationController
+
+  def new
+    @family = Family.new
+    authorize @family
+  end
+
   def create
+    @family = Family.new(family_params)
+    authorize @family
+
+    if @family.save
+      redirect_to root_path, notice: 'Votre famille a été créée.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def family_params
+    params.require(:family).permit(:name)
   end
 end
