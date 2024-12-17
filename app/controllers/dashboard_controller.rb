2b191439ebda922cb01f7ca1fe4ba4e1324baaf0
Guillaume Fournier <fournierg01@gmail.com>
2024-12-17 09:18:18 +0100

    hophop

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   before_action :set_family
 
   def index
+    @children = Child.includes(:tasks).where(family_id: @family.id)
   end
 
   def view
---
+++ app/javascript/controllers/graph_modal_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="graph-modal"
+export default class extends Controller {
+  static targets = ["modal"];
+  connect() {
+    //console.log("connecté")
+  }
+  open() {
+    //console.log("connecté");
+    //console.log("Targets :", this.hasModalTarget);
+    this.modalTarget.classList.remove("hidden");
+    this.modalTarget.classList.add("open");
+    console.log("Classes actuelles :", this.modalTarget.classList.value);
+  }
+
+  close() {
+    this.modalTarget.classList.remove("open");
+    setTimeout(() => this.modalTarget.classList.add("hidden"), 300);
+  }
+}
